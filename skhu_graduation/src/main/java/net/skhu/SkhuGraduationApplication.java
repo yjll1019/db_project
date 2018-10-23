362f05c7a550b792047943cb0bd20047480355d9
MyoungHyeYeon <hyeyeon2964@hanmail.net>
2018-10-23 16:31:08 +0900

    skhu_graduation

---
+++ skhu_graduation/src/main/java/net/skhu/SkhuGraduationApplication.java
@@ -0,0 +1,12 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuGraduationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkhuGraduationApplication.class, args);
+	}
+}
