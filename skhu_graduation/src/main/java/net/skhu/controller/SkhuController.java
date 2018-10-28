db28634a280134c333043a099b9c9f04ba59e8be
KimJye <eoeogudgud@naver.com>
2018-10-28 16:08:10 +0900

    no message

---
+++ skhu_graduation/src/main/java/net/skhu/controller/SkhuController.java
@@ -0,0 +1,24 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class SkhuController {
+	
+	@RequestMapping("nonmember")
+    public String list(Model model) {
+       
+        return "nonmember";
+    }
+
+
+	
+}
