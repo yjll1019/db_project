a550a31422efba4dd6128af06ac7e172ae8dc3e2
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-11-04 02:33:24 +0900

    예지 : ServletInitializer 되돌려놓음
    예지 : ServletInitializer 되돌려놓음

--- skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
+++ skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
@@ -1,11 +1,8 @@
 package net.skhu;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
