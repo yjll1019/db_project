5f39f63b752b2fdf3a64c64544969c56cb742d89
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-11-04 02:16:41 +0900

    예지 : 서버
    예지 : 서버

--- skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
+++ skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
@@ -1,11 +1,8 @@
 package net.skhu;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import javafx.application.Application;
 
 
 @SpringBootApplication
@@ -15,8 +12,5 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SkhuGraduationApplication.class);
 	}
-	
-	public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+
 }
