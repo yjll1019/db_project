d4e2b338025cbc36441e8c6506d3cb113b28796e
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-11-03 20:16:56 +0900

    예지 : 임시비번 발급 시 소문자 추가
    예지 : 임시비번 발급 시 소문자 추가

--- skhu_graduation/src/main/java/net/skhu/util/FindUtil.java
+++ skhu_graduation/src/main/java/net/skhu/util/FindUtil.java
@@ -5,7 +5,8 @@ public class FindUtil {
 		char[] charSet = new char[] {'0','1','2','3','4','5','6','7','8','9',
 									 'A','B','C','D','E','F','G','H','I','J',
 									 'K','L','M','N','O','P','Q','R','S','T',
-									 'U','V','W','X','Y','Z'};
+									 'U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s'
+									 ,'t','u','v','w','x','y','z'};
 		int index=0;
 
 		StringBuffer sb = new StringBuffer();
