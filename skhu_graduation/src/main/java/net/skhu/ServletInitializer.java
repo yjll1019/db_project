9aff8aebaf13cbc88295cc8702c44be9fc7ac333
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-11-04 02:10:45 +0900

    예지 : 어노테이션 추가
    예지 : 어노테이션 추가

--- skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
+++ skhu_graduation/src/main/java/net/skhu/ServletInitializer.java
@@ -1,13 +1,22 @@
 package net.skhu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javafx.application.Application;
+
+
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SkhuGraduationApplication.class);
 	}
-
+	
+	public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
