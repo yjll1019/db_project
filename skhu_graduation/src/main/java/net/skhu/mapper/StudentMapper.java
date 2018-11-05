801782dd750316d45f2976c10e96078e7778f1d5
KimJye <eoeogudgud@naver.com>
2018-11-05 21:33:00 +0900

    지혜 : update studentMapper

--- skhu_graduation/src/main/java/net/skhu/mapper/StudentMapper.java
+++ skhu_graduation/src/main/java/net/skhu/mapper/StudentMapper.java
@@ -3,7 +3,6 @@ package net.skhu.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import net.skhu.dto.Student;
 
@@ -12,6 +11,7 @@ public interface StudentMapper {
 
 	void insert(Student s); //회원가입 조건 충족 시 insert
 	Student stuInfo(String userId);
+	Student findOneWithUser(String userId);
 	List<Student> findAllWithUser();
 	List<Student> findById(String userId);
 	List<Student> findByName(String userName);
