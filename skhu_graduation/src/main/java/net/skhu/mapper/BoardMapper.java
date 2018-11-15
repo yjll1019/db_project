197b8f03a4aca228cea65d144a4d669e868c317e
HYEJI <lwiiiiii@naver.com>
2018-11-15 18:56:52 +0900

    혜지: 게시글 목록 페이지 구현(페이지네이션 나중에 추가할 예정)

---
+++ skhu_graduation/src/main/java/net/skhu/mapper/BoardMapper.java
@@ -0,0 +1,27 @@
+package net.skhu.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import net.skhu.dto.Board;
+
+@Mapper
+public interface BoardMapper {
+	Board findOne(int boardId);
+	List<Board> findAll();
+	List<Board> findByName(String userName);
+	List<Board> findByTitle(String title);
+	
+	void insertNotice(Board board);
+	void insertQuestion(Board board);
+	void insertAnswer(Board board);
+	
+	void updateNotice(Board board);
+	void updateQuestion(Board board);
+	void updateAnswer(Board board);
+	
+	void deleteNotice(int boardId);
+	void deleteQuestion(int boardId);
+	void deleteAnswer(int boardId);
+}
