f051fdf3d97c673373ae059ee277632b6b3f9411
KimJye <eoeogudgud@naver.com>
2018-11-26 09:50:33 +0900

    지혜 : 관리자 학생 조치사항

---
+++ skhu_graduation/src/main/java/net/skhu/dto/Record.java
@@ -0,0 +1,20 @@
+package net.skhu.dto;
+
+public class Record {
+	String studentId;
+	String content;
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+}
